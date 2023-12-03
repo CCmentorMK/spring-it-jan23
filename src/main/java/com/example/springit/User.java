c6120bdd25d24a32d1b8d53e1c8c2de3e7d1390f
MikiKru <michal_kruczkowski@o2.pl>
2023-12-03 12:50:44 +0100

    Spring TESTS

---
+++ src/main/java/com/example/springit/User.java
@@ -0,0 +1,31 @@
+package com.example.springit;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
---
+++ src/test/java/com/example/springit/UserIntegrationTest.java
@@ -0,0 +1,83 @@
+package com.example.springit;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(locations = "classpath:application-test.properties")
+@AutoConfigureMockMvc
+public class UserIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    public void testFullIntegrationFlow() throws Exception {
+        // Create a new user
+        User newUser = new User();
+        newUser.setFirstName("John");
+        newUser.setLastName("Doe");
+
+        ResultActions resultActions = mockMvc.perform(post("/api/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(newUser)));
+
+        resultActions
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", notNullValue()))
+                .andExpect(jsonPath("$.firstName", is("John")))
+                .andExpect(jsonPath("$.lastName", is("Doe")));
+
+        String content = resultActions.andReturn().getResponse().getContentAsString();
+        User createdUser = objectMapper.readValue(content, User.class);
+
+        // Retrieve the created user by ID
+        mockMvc.perform(get("/api/users/{id}", createdUser.getId()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(createdUser.getId().intValue())))
+                .andExpect(jsonPath("$.firstName", is("John")))
+                .andExpect(jsonPath("$.lastName", is("Doe")));
+
+        // Update the created user
+        User updatedUser = new User();
+        updatedUser.setFirstName("UpdatedFirstName");
+        updatedUser.setLastName("UpdatedLastName");
+
+        mockMvc.perform(put("/api/users/{id}", createdUser.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(updatedUser)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(createdUser.getId().intValue())))
+                .andExpect(jsonPath("$.firstName", is("UpdatedFirstName")))
+                .andExpect(jsonPath("$.lastName", is("UpdatedLastName")));
+
+        // Verify the updated user
+        mockMvc.perform(get("/api/users/{id}", createdUser.getId()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(createdUser.getId().intValue())))
+                .andExpect(jsonPath("$.firstName", is("UpdatedFirstName")))
+                .andExpect(jsonPath("$.lastName", is("UpdatedLastName")));
+
+        // Delete the created user
+        mockMvc.perform(delete("/api/users/{id}", createdUser.getId()))
+                .andExpect(status().isOk());
+
+        // Verify that the user is deleted
+        mockMvc.perform(get("/api/users/{id}", createdUser.getId()))
+                .andExpect(status().isOk());
+    }
+}
